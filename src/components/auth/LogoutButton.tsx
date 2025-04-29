d9392b198bcb53eb8fe9fba13bfefc9c59520480
KRISHNAKUMARPS2002 <krishnakumarkichu2002@gmail.com>
2025-04-29 18:15:12 +0530

    typescript error fixed

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-import { IoClose } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 const LogoutButton: React.FC = () => {
   const { logout } = useAuth();
@@ -42,19 +42,32 @@ const LogoutButton: React.FC = () => {
       {/* Logout Confirmation */}
       {logoutPopUp && (
         <div className="fixed top-[70px] right-2 w-[300px] z-20 h-[150px] bg-gradient-to-br from-[#ffffff] to-[#ffffff] rounded-2xl backdrop-blur-2xl shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] py-5">
-          <div onClick={() => setLogoutPopUp(!logoutPopUp)} className="text-[#000] cursor-pointer absolute top-2 right-2">
-            <IoClose/>
+          <div
+            onClick={() => setLogoutPopUp(!logoutPopUp)}
+            className="text-[#000] cursor-pointer absolute top-2 right-2"
+          >
+            <IoMdClose />
           </div>
-          <div className="text-[#000] mb-5 font-semibold text-center">Are you sure want to logout</div>
+          <div className="text-[#000] mb-5 font-semibold text-center">
+            Are you sure want to logout
+          </div>
           <div className="flex justify-center  items-center gap-10">
-            <button 
+            <button
               onClick={handleLogout}
-              disabled={isLoggingOut} className="px-6 py-2 bg-[#f00] rounded-lg cursor-pointer shadow-sm font-medium">Logout</button>
-            <button onClick={() => setLogoutPopUp(!logoutPopUp)} className="px-6 py-2 cursor-pointer bg-[#cafffc] rounded-lg shadow-sm text-[#000] font-medium">Cancel</button>
+              disabled={isLoggingOut}
+              className="px-6 py-2 bg-[#f00] rounded-lg cursor-pointer shadow-sm font-medium"
+            >
+              Logout
+            </button>
+            <button
+              onClick={() => setLogoutPopUp(!logoutPopUp)}
+              className="px-6 py-2 cursor-pointer bg-[#cafffc] rounded-lg shadow-sm text-[#000] font-medium"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       )}
-        
     </div>
   );
 };
